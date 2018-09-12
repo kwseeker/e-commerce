ed3245c667cd5cc74c2ae35fadf81f0c8ff2e90b
Arvin Lee <xiaohuileee@gmail.com>
2018-09-13 02:41:07 +0800

    shipping module realization

--- src/main/java/top/kwseeker/emall/controller/consume/ShippingController.java
+++ src/main/java/top/kwseeker/emall/controller/consume/ShippingController.java
@@ -3,9 +3,14 @@ package top.kwseeker.emall.controller.consume;
 import com.github.pagehelper.PageInfo;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import top.kwseeker.emall.common.Const;
+import top.kwseeker.emall.common.ResponseCode;
 import top.kwseeker.emall.common.ServerResponse;
 import top.kwseeker.emall.pojo.Shipping;
+import top.kwseeker.emall.pojo.User;
+import top.kwseeker.emall.service.IShippingService;
 
 import javax.servlet.http.HttpSession;
 
@@ -17,41 +22,61 @@ import javax.servlet.http.HttpSession;
 @Api(description = "收货信息")
 public class ShippingController {
 
+    @Autowired
+    IShippingService iShippingService;
+
     /**
      * @param shipping 收货信息对象
      */
     @PostMapping("add")
     @ApiOperation("添加收货信息")
     public ServerResponse add(HttpSession session, Shipping shipping){
-        return null;
+        User user = (User)session.getAttribute(Const.CURRENT_USER);
+        if(user ==null){
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),ResponseCode.NEED_LOGIN.getDesc());
+        }
+        return iShippingService.add(user.getId(), shipping);
     }
-
 
     @DeleteMapping("del")
     @ApiOperation("删除收货信息")
     public ServerResponse del(HttpSession session,Integer shippingId){
-        return null;
+        User user = (User)session.getAttribute(Const.CURRENT_USER);
+        if(user ==null){
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),ResponseCode.NEED_LOGIN.getDesc());
+        }
+        return iShippingService.del(user.getId(), shippingId);
     }
 
     @PostMapping("update")
     @ApiOperation("更新收货信息")
     public ServerResponse update(HttpSession session,Shipping shipping){
-        return null;
+        User user = (User)session.getAttribute(Const.CURRENT_USER);
+        if(user ==null){
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),ResponseCode.NEED_LOGIN.getDesc());
+        }
+        return iShippingService.update(user.getId(), shipping);
     }
-
 
     @GetMapping("select")
     @ApiOperation("参看某条收货信息")
     public ServerResponse<Shipping> select(HttpSession session,Integer shippingId){
-        return null;
+        User user = (User)session.getAttribute(Const.CURRENT_USER);
+        if(user ==null){
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),ResponseCode.NEED_LOGIN.getDesc());
+        }
+        return iShippingService.select(user.getId(), shippingId);
     }
 
-
-    @GetMapping("list.do")
+    @GetMapping("list")
     @ApiOperation("分页查看所有收货信息列表")
     public ServerResponse<PageInfo> list(@RequestParam(value = "pageNum",defaultValue = "1") int pageNum,
                                          @RequestParam(value = "pageSize",defaultValue = "10")int pageSize,
                                          HttpSession session){
-        return null;
+        User user = (User)session.getAttribute(Const.CURRENT_USER);
+        if(user ==null){
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),ResponseCode.NEED_LOGIN.getDesc());
+        }
+        return iShippingService.list(user.getId(),pageNum,pageSize);
     }
 }
